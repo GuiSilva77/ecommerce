7bfdb7974d7476dc2342519ccd38945bf49cd0f7
Guilherme Silva <guilherme92325@gmail.com>
2024-04-21 20:08:19 -0300

    Adicionar exceções

---
+++ app/exceptions/resource_not_found.ts
@@ -0,0 +1,11 @@
+import { Exception } from '@adonisjs/core/exceptions'
+import { HttpContext } from '@adonisjs/core/http'
+
+export default class ResourceNotFoundException extends Exception {
+  static status = 404
+  static code = 'RESOURCE_NOT_FOUND'
+
+  async handle(error: this, ctx: HttpContext) {
+    ctx.response.status(error.status).send(error.message)
+  }
+}
