7f3fe269e7759ed0b60d82e83c00872d2ddb676b
Guilherme Silva <guilherme92325@gmail.com>
2024-04-21 22:08:45 -0300

    Proteger as rotas de Usuario com token

--- app/routes/usuarios/main.ts
+++ app/routes/usuarios/main.ts
@@ -8,9 +8,19 @@
 */
 
 const UsuariosController = () => import('#controllers/usuarios_controller')
+import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
-router.get('/usuarios/:id', [UsuariosController, 'findOne'])
 router.post('/usuarios', [UsuariosController, 'criar'])
-router.delete('/usuarios/:id', [UsuariosController, 'deletar'])
-router.put('/usuarios/:id', [UsuariosController, 'atualizar'])
+
+router
+  .group(() => {
+    router.get('/usuarios/:id', [UsuariosController, 'findOne'])
+    router.delete('/usuarios/:id', [UsuariosController, 'deletar'])
+    router.put('/usuarios/:id', [UsuariosController, 'atualizar'])
+  })
+  .use(
+    middleware.auth({
+      guards: ['api'],
+    })
+  )
