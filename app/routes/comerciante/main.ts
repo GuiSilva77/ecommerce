ca52d6d5d842fb9c984a19f3deb2c98ef96622da
luisttkfps <luis.fernandops582@gmail.com>
2024-05-11 14:17:14 -0300

    Enpoints logoff

--- app/routes/comerciante/main.ts
+++ app/routes/comerciante/main.ts
@@ -1,5 +1,6 @@
 import AuthComercianteController from '#controllers/Comerciante/Auth'
 import MainComercianteController from '#controllers/Comerciante/Main'
+import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
 
 router.post('/comerciantes', [MainComercianteController, 'criar'])
@@ -7,3 +8,5 @@ router.get('/comerciantes/:id', [MainComercianteController, 'index'])
 router.post('/comerciantes/auth', [AuthComercianteController, 'criar'])
 router.put('/comerciantes', [MainComercianteController, 'alterar'])
 router.delete('/comerciantes', [MainComercianteController, 'deletar'])
+router.delete('/comerciantes/auth', [AuthComercianteController, 'deletar']).use(middleware.auth({guards:['comerciante']}))
+
